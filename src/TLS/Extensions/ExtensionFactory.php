e50e811a67cff51edefad07369a3bf2fda19a011
nwnisworking <66354122+nwnisworking@users.noreply.github.com>
2025-12-20 03:52:39 +0000

    Added extension registry

---
+++ src/TLS/Extensions/ExtensionFactory.php
@@ -0,0 +1,20 @@
+<?php
+namespace nwniscoding\TLS\Extensions;
+
+use InvalidArgumentException;
+use nwniscoding\TLS\Enums\ExtensionEnum;
+
+class ExtensionFactory{
+  public static function createExtension(int $ext, string $data): Extension{
+    $ext = ExtensionEnum::tryFrom($ext);
+
+    if($ext === null) throw new InvalidArgumentException("Unknown extension type: $ext");
+
+    return match($ext){
+      ExtensionEnum::RENEGOTIATION_INFO => RenegotiationInfoExtension::decode($data),
+      ExtensionEnum::EXTENDED_MASTER_SECRET => ExtendedMasterSecretExtension::decode($data),
+      ExtensionEnum::ENCRYPT_THEN_MAC => EncryptThenMacExtension::decode($data),
+      default => new UnknownExtension($ext, $data),
+    };
+  }
+}
