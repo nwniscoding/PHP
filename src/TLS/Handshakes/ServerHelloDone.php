a7831af0300b519e1dfe515d501b000a79b7e669
nwnisworking <66354122+nwnisworking@users.noreply.github.com>
2025-12-20 05:57:39 +0000

    Added ServerHelloDone

---
+++ src/TLS/Handshakes/ServerHelloDone.php
@@ -0,0 +1,18 @@
+<?php
+namespace nwniscoding\TLS\Handshakes;
+
+use nwniscoding\TLS\Enums\HandshakeEnum;
+
+class ServerHelloDone extends Handshake{
+  public function __construct(){
+    parent::__construct(HandshakeEnum::SERVER_HELLO_DONE);
+  }
+
+  public function encode(): string{
+    return pack('C', $this->type->value) . str_repeat("\0", 3);
+  }
+
+  public static function decode(string $data): self{
+    return new self();
+  }
+}
