99c0b779199964434b28d9c755ebd7f25a90c76c
nwnisworking <nwnisworking@gmail.com>
2025-12-31 20:49:51 +0800

    Added tests

--- src/TLS/configs/server.bat
+++ src/TLS/configs/server.bat
@@ -1,10 +1,10 @@
 @echo off
 openssl s_server ^
  -accept 9000 ^
- -cert ec.crt ^
- -key ec.key ^
- -cert rsa.crt ^
- -key rsa.key ^
+ -cert keys/ec.crt ^
+ -key keys/ec.key ^
+ -cert keys/rsa.crt ^
+ -key keys/rsa.key ^
  -psk 1a2b3c4d5e6f7081 ^
  -psk_identity myidentity ^
  -msg ^
