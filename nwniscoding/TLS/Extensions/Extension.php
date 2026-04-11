1a62931a3d522d5e18d681944fcdfea138dbc08c
nwnisworking <nwnisworking@gmail.com>
2026-04-11 12:30:22 +0800

    Refactor Extension class to include HandshakeType in encode and decode methods

--- nwniscoding/TLS/Extensions/Extension.php
+++ nwniscoding/TLS/Extensions/Extension.php
@@ -1,23 +1,23 @@
 <?php
 namespace nwniscoding\TLS\Extensions;
 
+use nwniscoding\TLS\Enums\HandshakeType;
 use function strlen;
 
 use nwniscoding\IO\BufferReader;
 use nwniscoding\IO\BufferWriter;
 use nwniscoding\TLS\Enums\ExtensionType;
-use nwniscoding\TLS\Sessions\Session;
 
 abstract readonly class Extension{
   abstract public function getType() : ExtensionType;
 
-  abstract protected function encode() : string;
+  abstract protected function encode(HandshakeType $type) : string;
 
-  abstract public static function decode(BufferReader $reader, string $class);
+  abstract public static function decode(BufferReader $reader, HandshakeType $type) : self;
 
-  public function __tostring() : string{
+  public function toBinary(HandshakeType $type) : string{
     $writer = new BufferWriter();
-    $encode = $this->encode();
+    $encode = $this->encode($type);
 
     $writer->writeUint16($this->getType()->value);
     $writer->writeUint16(strlen($encode));
